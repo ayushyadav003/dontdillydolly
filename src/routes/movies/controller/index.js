4dcbf8d3083f54260599949b9185a135edfadda0
Ayush <ayush.yadav.100100@gmail.com>
2022-12-08 21:00:04 +0530

    persist data onload fix + minor seo addition

--- src/routes/movies/controller/index.js
+++ src/routes/movies/controller/index.js
@@ -5,14 +5,15 @@ dotenv.config();
 
 export const getMovies = async (req, res) => {
   try {
-    const page = req.query.page;
+    var query = req.query;
+    query.api_key = process.env.API_KEY;
+    // query?.main ? delete person.main : null;query
     const options = {
       method: "GET",
-      url: `https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&&with_watch_monetization_types=flatrate`,
-      params: {
-        page: page,
-        api_key: process.env.API_KEY,
-      },
+      url: query.main
+        ? `https://api.themoviedb.org/3/movie/${query.main}`
+        : `https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&&with_watch_monetization_types=flatrate`,
+      params: query,
     };
     const { data } = await axios(options);
     res.status(200).json({
--- src/routes/tvSeries/controller/index.js
+++ src/routes/tvSeries/controller/index.js
@@ -5,14 +5,14 @@ dotenv.config();
 
 export const getSeries = async (req, res) => {
   try {
-    const page = req.query.page;
+    var query = req.query;
+    query.api_key = process.env.API_KEY;
     const options = {
       method: "GET",
-      url: `https://api.themoviedb.org/3/discover/tv`,
-      params: {
-        page: page,
-        api_key: process.env.API_KEY,
-      },
+      url: query.main
+        ? `https://api.themoviedb.org/3/tv/${query.main}`
+        : `https://api.themoviedb.org/3/discover/tv`,
+      params: query,
     };
     const { data } = await axios(options);
     res.status(200).json({
